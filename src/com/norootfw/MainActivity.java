0247382fe55f2e47075e3fb708460eb70da0684d
Maksim Dmitriev <maksim.a.dmitriev@gmail.com>
2014-10-05 21:10:44 +0400

    Initial commit. There are syntax errors in the code

---
+++ src/com/norootfw/MainActivity.java
@@ -0,0 +1,57 @@
+package com.norootfw;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class MainActivity extends Activity implements OnClickListener {
+
+    private static final int ENBLE_FIREWALL_REQ_CODE = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = FirewallService.prepare(this);
+        if (intent != null) {
+            startActivityForResult(intent, ENBLE_FIREWALL_REQ_CODE);
+        } else {
+            onActivityResult(ENBLE_FIREWALL_REQ_CODE, RESULT_OK, null);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int request, int result, Intent data) {
+        if (result == RESULT_OK) {
+            Intent intent = new Intent(this, FirewallService.class);
+            startService(intent);
+        }
+    }
+}
