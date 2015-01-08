8fc7961148ac02c0119d74612520ab758606cc2b
Maksim Dmitriev <maksim.a.dmitriev@gmail.com>
2015-01-08 13:14:05 +0300

    Fix SIGABRT. However, there is no guarantee that everything works properly

--- jni/no_root_firewall_jni.c
+++ jni/no_root_firewall_jni.c
@@ -1,7 +1,7 @@
 #include "no_root_firewall_jni.h"
 #include "lwip/ip.h"
 #include "lwip/pbuf.h"
-#include "arch/cc.h"
+#include "lwip/mem.h"
 
 JNIEXPORT jint JNICALL Java_com_norootfw_NoRootFwNative_ip_1input(JNIEnv *env,
 		jclass clazz, jbyteArray packet, jint payload_length) {
@@ -11,6 +11,10 @@ JNIEXPORT jint JNICALL Java_com_norootfw_NoRootFwNative_ip_1input(JNIEnv *env,
 
 	 Design and Implementation of the lwIP TCP/IP Stack.
 	 */
+	mem_init();
 	struct pbuf *buf = pbuf_alloc(PBUF_LINK, payload_length, PBUF_RAM);
+	// TODO: I probably should call mem_free() here.
+	// Maksim Dmitriev
+	// January 8, 2015
 	return 0;
 }
